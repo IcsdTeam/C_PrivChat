d3bd1df3ead40ad5032135678e845074c07f209e
IcsdTeam <44102964+IcsdTeam@users.noreply.github.com>
2018-10-14 03:22:34 +0300

    Fixed Name convention

--- src/client/Controllers/Controllers_PUC/CtrlPUC_Main.java
+++ src/client/Controllers/Controllers_PUC/CtrlPUC_Main.java
@@ -4,13 +4,12 @@ import client.Controllers.AbstractController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CtrlPUC_Main extends AbstractController implements Initializable {
     @FXML
-    VBox vboxCh1_SignIn;
+    VBox vboxPUC_Main;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
